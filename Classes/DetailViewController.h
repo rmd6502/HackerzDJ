72c39e2df2c6eb36f1ea7bd4698202fc8b0326a7
Robert Diamond <robert.diamond@patch.com>
2011-04-29 23:25:06 -0400

    add video detail view

---
+++ Classes/DetailViewController.h
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.h
+//  hackerdjz
+//
+//  Created by Robert Diamond on 4/29/11.
+//  Copyright 2011 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface DetailViewController : UIViewController {
+
+}
+
+@property (nonatomic,copy) NSDictionary *details;
+
+@end
